5e074f0b736cdc82e02c5298bc4e990f0ecb9dfd
Alexandre Radicchi <alex.radi@gmail.com>
2020-03-03 13:46:04 +0000

    minor

--- get_ecdc_page.py
+++ get_ecdc_page.py
@@ -1,7 +1,18 @@
+'''
+A script for downloading the ecdc page with status on coronavirus.
+'''
+import requests
+from bs4 import BeautifulSoup
 import datetime as dt
 import wget
 
 ecdc_uri = "https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases"
 out_folder = "ecdc_pages"
 
-wget.download(ecdc_uri,"%s/ecdc_worldwide_%s.html" % (out_folder,dt.datetime.today().strftime("%Y%m%d")))
+r = requests.get(ecdc_uri)
+soup = BeautifulSoup(r.text,'html.parser')
+print("-> report title:", soup.title.text)
+
+out_file = "%s/ecdc_worldwide_%s.html" % (out_folder,dt.datetime.today().strftime("%Y%m%d"))
+print("-> saving to:", out_file)
+wget.download(ecdc_uri,out_file)
